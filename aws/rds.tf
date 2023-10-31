30965a9291fae4c61b1beeaec95a1a3888d28562
DaedalusG <warrenbruceg@gmail.com>
2023-10-31 16:29:21 -0700

    correct security group issues, put everything in same VPC

--- aws/rds.tf
+++ aws/rds.tf
@@ -3,61 +3,49 @@ resource "aws_db_instance" "sourcegraph" {
   count = var.create_rds ? 1 : 0
 
   engine         = "postgres"
-  engine_version = "12.7"
+  engine_version = "12.16"
 
-  # Use large instance type
-  instance_class = var.frontend_db_instance_class
-  # Provision storage for all DBs
+  instance_class    = var.frontend_db_instance_class
   allocated_storage = var.frontend_db_allocated_storage
 
-  # Create frontend databases
-  db_name  = var.frontend_db_name
-  username = var.frontend_db_username
-  password = var.frontend_db_password
+  db_name             = var.frontend_db_name
+  username            = var.frontend_db_username
+  password            = var.frontend_db_password
+  publicly_accessible = true
 
-  # Allow access from Sourcegraph
-  publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.node_group_one.id]
-
+  vpc_security_group_ids = [aws_security_group.rds.id]
 }
 
 resource "aws_db_instance" "codeintel" {
   count = var.create_rds ? 1 : 0
 
   engine         = "postgres"
-  engine_version = "12.7"
+  engine_version = "12.16"
 
-  # Use large instance type
-  instance_class = var.codeintel_db_instance_class
-  # Provision storage for all DBs
+  instance_class    = var.codeintel_db_instance_class
   allocated_storage = var.codeintel_db_allocated_storage
 
-  db_name  = var.codeintel_db_name
-  username = var.codeintel_db_username
-  password = var.codeintel_db_password
+  db_name             = var.codeintel_db_name
+  username            = var.codeintel_db_username
+  password            = var.codeintel_db_password
+  publicly_accessible = true
 
-  # Allow access from Sourcegraph
-  publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.node_group_one.id]
+  vpc_security_group_ids = [aws_security_group.rds.id]
 }
 
 resource "aws_db_instance" "codeinsights" {
   count = var.create_rds ? 1 : 0
 
   engine         = "postgres"
-  engine_version = "12.7"
+  engine_version = "12.16"
 
-  # Use large instance type
-  instance_class = var.codeinsights_db_instance_class
-  # Provision storage for all DBs
+  instance_class    = var.codeinsights_db_instance_class
   allocated_storage = var.codeinsights_db_allocated_storage
 
-  db_name  = var.codeinsights_db_name
-  username = var.codeinsights_db_username
-  password = var.codeinsights_db_password
+  db_name             = var.codeinsights_db_name
+  username            = var.codeinsights_db_username
+  password            = var.codeinsights_db_password
+  publicly_accessible = true
 
-  # Allow access from Sourcegraph
-  publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.node_group_one.id]
+  vpc_security_group_ids = [aws_security_group.rds.id]
 }
-
--- aws/security-groups.tf
+++ aws/security-groups.tf
@@ -12,3 +12,15 @@ resource "aws_security_group" "node_group_one" {
     ]
   }
 }
+
+resource "aws_security_group" "rds" {
+  name_prefix = "rds"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.node_group_one.id]
+  }
+}
